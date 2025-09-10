4afca96af724eb9bbbf20939331bcae6dabe6132
unknown <pauovictorpinheiro998663264@gmail.com>
2025-09-10 18:09:53 -0300

    Client

--- Models/Client/user.py
+++ Models/Client/user.py
@@ -6,13 +6,37 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
-    def __init__(self, id, email, password):
-        self.id = id
+    def __init__(self, email, password, id=None):
+        """
+        Construtor da classe User.
+        
+        Args:
+            email (str): Email do usuário
+            password (str): Senha hash do usuário
+            id (int, optional): ID do usuário. Se None, será gerado automaticamente pelo banco
+        """
+        if id is not None:
+            self.id = id
         self.email = email
         self.password = password
     
     def to_dict(self):
-        return{
+        """
+        Converte o objeto User para dicionário (útil para APIs JSON).
+        
+        Returns:
+            dict: Dicionário com os dados do usuário (sem a senha)
+        """
+        return {
             "id": self.id, 
             "email": self.email, 
         }
+    
+    def __repr__(self):
+        """
+        Representação string do objeto User (útil para debug).
+        
+        Returns:
+            str: Representação do usuário
+        """
+        return f'<User {self.email}>'

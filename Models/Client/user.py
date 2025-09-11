e5f710cfae31f0a36661da911dde80d8073c5f10
Paulo Victor Pinheiro <pauovictorpinheiro998663264@gmail.com>
2025-09-11 00:00:56 -0300

    Models

--- Models/Client/user.py
+++ Models/Client/user.py
@@ -7,36 +7,17 @@ class User(db.Model, UserMixin):
     password = db.Column(db.LargeBinary, nullable=False)  # Mudança aqui: LargeBinary para armazenar bytes
 
     def __init__(self, email, password, id=None):
-        """
-        Construtor da classe User.
-
-        Args:
-            email (str): Email do usuário
-            password (bytes): Senha hash do usuário (em bytes)
-            id (int, optional): ID do usuário. Se None, será gerado automaticamente pelo banco
-        """
         if id is not None:
             self.id = id
         self.email = email
         self.password = password
 
     def to_dict(self):
-        """
-        Converte o objeto User para dicionário (útil para APIs JSON).
-
-        Returns:
-            dict: Dicionário com os dados do usuário (sem a senha)
-        """
         return {
             "id": self.id,
             "email": self.email,
         }
 
     def __repr__(self):
-        """
-        Representação string do objeto User (útil para debug).
 
-        Returns:
-            str: Representação do usuário
-        """
         return f'<User {self.email}>'
